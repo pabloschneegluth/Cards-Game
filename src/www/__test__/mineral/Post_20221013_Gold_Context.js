3b79ff02570f5e0b88ecd939a66b09420ccadd7a
Pablo Fernández <pabloft99@gmail.com>
2022-10-16 18:07:01 +0200

    ww

---
+++ src/www/__test__/mineral/Post_20221013_Gold_Context.js
@@ -0,0 +1,76 @@
+import { getByTestId, screen } from "@testing-library/react";
+import { mainView } from "../main";
+import { waitForEnterTheGame, waitForReloadGame } from "../game/actions";
+import { waitForEndMoon } from "../moon/actions";
+
+export class Post_20221013_Gold_Context {
+  async beforeTest() {
+    // Do your setup here
+    await waitForEnterTheGame();
+
+    throw new Error(
+      "Please, review the implementation of beforeTest() and remove this exception when it is correct."
+    );
+  }
+
+  async givenANewGameWithAStackOfNSCardsAndNSCards(n1, s1, n2, s2) {
+    // text:  * Given a new game with a stack of 1 "Woods Stroll Idea" cards and 1 "Villager" cards.
+    // code: await this.givenANewGameWithAStackOfNSCardsAndNSCards(1, "Woods Stroll Idea", 1, "Villager")
+    // hint: Post_20220727_IHaveAnIdeaToTakeAStrollInTheWoodAndFindRandomThings_Context.givenANewGameWithAStackOfNSCardsAndNSCards
+
+    await waitForReloadGame();
+
+    throw new Error(
+      "The method givenANewGameWithAStackOfNSCardsAndNSCards(n1, s1, n2, s2) is not implemented yet."
+    );
+  }
+
+  async theSMayCreateASCard(s1, s2) {
+    // text:  * The "Woods Stroll Idea" may create a "Gold" card.
+    // code: await this.theSMayCreateASCard("Woods Stroll Idea", "Gold")
+    // hint: Post_20220727_IHaveAnIdeaToTakeAStrollInTheWoodAndFindRandomThings_Context.theSMayCreateASCard
+
+    throw new Error(
+      "The method theSMayCreateASCard(s1, s2) is not implemented yet."
+    );
+  }
+
+  async givenThatTheOddsAreThatWeWillGetASFromTheSCard(s1, s2) {
+    // text:  * Given that the odds are that we will get a "Gold" from the "Woods Stroll Idea" card.
+    // code: await this.givenThatTheOddsAreThatWeWillGetASFromTheSCard("Gold", "Woods Stroll Idea")
+    // hint: Post_20220727_IHaveAnIdeaToTakeAStrollInTheWoodAndFindRandomThings_Context.givenThatTheOddsAreThatWeWillGetASFromTheSCard
+
+    await waitForReloadGame();
+
+    throw new Error(
+      "The method givenThatTheOddsAreThatWeWillGetASFromTheSCard(s1, s2) is not implemented yet."
+    );
+  }
+
+  async endTheCurrentMoon() {
+    // text:  * End the current moon.
+    // code: await this.endTheCurrentMoon()
+    // hint: Post_20220727_IHaveAnIdeaToTakeAStrollInTheWoodAndFindRandomThings_Context.endTheCurrentMoon
+
+    await waitForEndMoon();
+
+    throw new Error("The method endTheCurrentMoon() is not implemented yet.");
+  }
+
+  async thereShouldBeNStacksOfNSNSAndNSCards(expected, n2, s1, n3, s2, n4, s3) {
+    // text:  * There should be 1 stacks of 1 "Woods Stroll Idea", 1 "Villager" and 1 "Gold" cards.
+    // code: await this.thereShouldBeNStacksOfNSNSAndNSCards(1, 1, "Woods Stroll Idea", 1, "Villager", 1, "Gold")
+    // hint: Post_20220727_IHaveAnIdeaToTakeAStrollInTheWoodAndFindRandomThings_Context.thereShouldBeNStacksOfNSNSAndNSCards
+
+    var actual = expected; // FIXME
+    expect(actual).toEqual(expected);
+
+    throw new Error(
+      "The method thereShouldBeNStacksOfNSNSAndNSCards(expected, n2, s1, n3, s2, n4, s3) is not implemented yet."
+    );
+  }
+
+  async afterTest() {
+    // Do your teardown here, if necessary
+  }
+}
