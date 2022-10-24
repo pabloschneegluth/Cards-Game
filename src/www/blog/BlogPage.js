9233aadff851ad90fdaded84a71cc6aae0bc3363
David Rodenas <david.rodenas+gt@gmail.com>
2022-10-24 15:30:07 +0200

    blog: add some statistics

--- src/www/blog/BlogPage.js
+++ src/www/blog/BlogPage.js
@@ -16,6 +16,10 @@ export function BlogPage() {
     <div className="blog">
       <h1>Blog</h1>
 
+      <PostCounts posts={posts} />
+      <br />
+      <PostsRanking posts={posts} />
+
       <h2>Implemented</h2>
       <div className="panel">
         {posts
@@ -37,6 +41,52 @@ export function BlogPage() {
             />
           ))}
       </div>
+    </div>
+  );
+}
+
+function PostCounts({ posts }) {
+  const implemented = posts.filter((post) => post.frontMatter.coder).length;
+  const unimplemented = posts.filter((post) => !post.frontMatter.coder).length;
+
+  return (
+    <div className="post-counts">
+      Counts:{" "}
+      <span className="post-counts--implemented">
+        {implemented} implemented
+      </span>
+      {", "}
+      <span className="post-counts--unimplemented">
+        {unimplemented} unimplemented
+      </span>
+    </div>
+  );
+}
+
+function PostsRanking({ posts }) {
+  const users = [
+    ...new Set(posts.map((post) => post.frontMatter.writer)),
+    "nobody",
+  ];
+  const stats = users.map((user) => ({
+    user,
+    writer: posts.filter((post) => post.frontMatter.writer === user).length,
+    coder: posts.filter((post) => post.frontMatter.coder === user).length,
+  }));
+  const sortedStats = stats.sort(
+    (a, b) => 999 * (b.coder - a.coder) + (b.writer - a.writer),
+  );
+
+  return (
+    <div className="posts-ranking">
+      <div>Authors:</div>
+      {sortedStats
+        .filter((state) => state.user !== "nobody")
+        .map((stat) => (
+          <div key={stat.user}>
+            - {stat.user}: {stat.coder} implemented, {stat.writer} written
+          </div>
+        ))}
     </div>
   );
 }
