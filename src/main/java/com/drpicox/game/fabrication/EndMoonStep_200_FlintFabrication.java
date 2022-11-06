0b1a151f1260600e47a6a512eec129b2238f1b34
Pabloooo6 <pschneegluth@edu.tecnocampus.cat>
2022-11-06 19:26:57 +0100

    Implemented backend: Flint

--- src/main/java/com/drpicox/game/fabrication/EndMoonStep_200_FlintFabrication.java
+++ src/main/java/com/drpicox/game/fabrication/EndMoonStep_200_FlintFabrication.java
@@ -3,9 +3,11 @@ package com.drpicox.game.fabrication;
 import com.drpicox.game.card.*;
 import com.drpicox.game.moon.EndMoonSettings;
 import com.drpicox.game.moon.EndMoonStep;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class EndMoonStep_200_FlintFabrication implements EndMoonStep {
 
     private final StackService stackService;
@@ -13,6 +15,7 @@ public class EndMoonStep_200_FlintFabrication implements EndMoonStep {
     private final CardService cardService;
 
     public EndMoonStep_200_FlintFabrication(StackService stackService, CardFactory cardFactory, CardService cardService) {
+        super();
         this.stackService = stackService;
         this.cardFactory = cardFactory;
         this.cardService = cardService;
@@ -31,11 +34,11 @@ public class EndMoonStep_200_FlintFabrication implements EndMoonStep {
         var villager = cards.stream().filter(card -> card.getName().equalsIgnoreCase("Villager")).findFirst();
         var stone = cards.stream().filter(card -> card.getName().equalsIgnoreCase("Stone")).findFirst();
 
-
         if (pickaxe.isEmpty() || villager.isEmpty() || stone.isEmpty()) {
             return;
         }
 
+        cardFactory.makeCard(new CardFactorySettings("Flint").withPosition(stone.get().getPosition()));
         cardFactory.makeCard(new CardFactorySettings("Flint").withPosition(stone.get().getPosition()));
         cardService.discardCard(stone.get());
     }
