0cff20f367c37e164d53ec2b2d7e241d7489b2f3
Pabloooo6 <pschneegluth@edu.tecnocampus.cat>
2022-11-06 19:13:29 +0100

    endmoonstep done

---
+++ src/main/java/com/drpicox/game/fabrication/EndMoonStep_200_FlintFabrication.java
@@ -0,0 +1,42 @@
+package com.drpicox.game.fabrication;
+
+import com.drpicox.game.card.*;
+import com.drpicox.game.moon.EndMoonSettings;
+import com.drpicox.game.moon.EndMoonStep;
+
+import java.util.List;
+
+public class EndMoonStep_200_FlintFabrication implements EndMoonStep {
+
+    private final StackService stackService;
+    private final CardFactory cardFactory;
+    private final CardService cardService;
+
+    public EndMoonStep_200_FlintFabrication(StackService stackService, CardFactory cardFactory, CardService cardService) {
+        this.stackService = stackService;
+        this.cardFactory = cardFactory;
+        this.cardService = cardService;
+    }
+
+    public void execute(EndMoonSettings settings) {
+        var stacks = stackService.findAllStack();
+
+        for (var stack : stacks) {
+            flintFabrication(stack.getCards());
+        }
+    }
+
+    private void flintFabrication(List<Card> cards) {
+        var pickaxe = cards.stream().filter(card -> card.getName().equalsIgnoreCase("Pickaxe")).findFirst();
+        var villager = cards.stream().filter(card -> card.getName().equalsIgnoreCase("Villager")).findFirst();
+        var stone = cards.stream().filter(card -> card.getName().equalsIgnoreCase("Stone")).findFirst();
+
+
+        if (pickaxe.isEmpty() || villager.isEmpty() || stone.isEmpty()) {
+            return;
+        }
+
+        cardFactory.makeCard(new CardFactorySettings("Flint").withPosition(stone.get().getPosition()));
+        cardService.discardCard(stone.get());
+    }
+}
