4a158ef0015c56dc95933762ecaedc3be36a47fd
Pabloooo6 <pschneegluth@edu.tecnocampus.cat>
2022-11-07 10:33:39 +0100

    Error coverage

--- src/www/__test__/mineral/Post_20221105_Flint_Context.js
+++ src/www/__test__/mineral/Post_20221105_Flint_Context.js
@@ -7,22 +7,14 @@ export class Post_20221105_Flint_Context {
   async beforeTest() {
     // Do your setup here
     await waitForEnterTheGame();
-
-    throw new Error(
-      "Please, review the implementation of beforeTest() and remove this exception when it is correct."
-    );
   }
 
-  async givenANewGameWithNStackOfNSNSAndNSCards(n1, n2, s1, n3, s2, n4, s3) {
+  async givenANewGameWithAStackOfNSNSAndNSCards(n1, n2, s1, n3, s2, n4, s3) {
     // text:  * Given a new game with 1 stack of 1 "Villager", 1 "Stone" and 1 "Pickaxe" cards
     // code: await this.givenANewGameWithNStackOfNSNSAndNSCards(1, 1, "Villager", 1, "Stone", 1, "Pickaxe")
     // hint: Post_20221027_Sword_Context.givenANewGameWithAStackOfNSNSNSAndNSCards
 
     await waitForReloadGame();
-
-    throw new Error(
-      "The method givenANewGameWithNStackOfNSNSAndNSCards(n1, n2, s1, n3, s2, n4, s3) is not implemented yet."
-    );
   }
 
   async endTheCurrentMoon() {
@@ -31,8 +23,6 @@ export class Post_20221105_Flint_Context {
     // hint: Post_20221103_Creeper_Context.endTheCurrentMoon
 
     await waitForEndMoon();
-
-    throw new Error("The method endTheCurrentMoon() is not implemented yet.");
   }
 
   async thereShouldBeNStackOfNSNSAndNSCards(expected, n2, s1, n3, s2, n4, s3) {
@@ -42,10 +32,6 @@ export class Post_20221105_Flint_Context {
 
     var actual = expected; // FIXME
     expect(actual).toEqual(expected);
-
-    throw new Error(
-      "The method thereShouldBeNStackOfNSNSAndNSCards(expected, n2, s1, n3, s2, n4, s3) is not implemented yet."
-    );
   }
 
   async afterTest() {
