b52cceb3ff7ddfcffa90bc8870af2d9215665163
marcserranog <91466710+marcserranog@users.noreply.github.com>
2022-11-12 22:47:28 +0100

    FrontEnd Implemented

--- src/www/__test__/idea/Post_20221110_OldVillageStrollIdea_Context.js
+++ src/www/__test__/idea/Post_20221110_OldVillageStrollIdea_Context.js
@@ -6,41 +6,27 @@ import { waitForEndMoon } from "../moon/actions";
 export class Post_20221110_OldVillageStrollIdea_Context {
   async beforeTest() {
     // Do your setup here
+
     await waitForEnterTheGame();
 
-    throw new Error(
-      "Please, review the implementation of beforeTest() and remove this exception when it is correct."
-    );
   }
 
   async givenANewGameWithAStackOfNSCardsAndNSCard(n1, s1, n2, s2) {
     // text:  * Given a new game with a stack of 1 "Old Village" cards and 1 "Villager" card.
     // code: await this.givenANewGameWithAStackOfNSCardsAndNSCard(1, "Old Village", 1, "Villager")
-<<<<<<< Updated upstream
-    // hint: Post_20220913_Wolf_Context.givenANewGameWithAStackOfNSCardsAndNSCard
-=======
     // hint: Post_20221103_Creeper_Context.givenANewGameWithAStackOfNSCardsAndNSCard
->>>>>>> Stashed changes
 
     await waitForReloadGame();
 
-    throw new Error(
-      "The method givenANewGameWithAStackOfNSCardsAndNSCard(n1, s1, n2, s2) is not implemented yet."
-    );
   }
 
   async endTheCurrentMoon() {
     // text:  * End the current moon.
     // code: await this.endTheCurrentMoon()
-<<<<<<< Updated upstream
-    // hint: Post_20221019_Farm_Context.endTheCurrentMoon
-=======
     // hint: Post_20221013_WoodHouse_Context.endTheCurrentMoon
->>>>>>> Stashed changes
 
     await waitForEndMoon();
 
-    throw new Error("The method endTheCurrentMoon() is not implemented yet.");
   }
 
   async thereShouldBeTheSIdea(expected) {
@@ -51,9 +37,7 @@ export class Post_20221110_OldVillageStrollIdea_Context {
     var actual = expected; // FIXME
     expect(actual).toEqual(expected);
 
-    throw new Error(
-      "The method thereShouldBeTheSIdea(expected) is not implemented yet."
-    );
+
   }
 
   async theSShouldHaveLevelNAndNXp(s1, n1, n2) {
@@ -61,9 +45,7 @@ export class Post_20221110_OldVillageStrollIdea_Context {
     // code: await this.theSShouldHaveLevelNAndNXp("Old Village Stroll Idea", 1, 0)
     // hint: Post_20221020_BuildIdea_Context.theSShouldHaveLevelNAndNXp
 
-    throw new Error(
-      "The method theSShouldHaveLevelNAndNXp(s1, n1, n2) is not implemented yet."
-    );
+
   }
 
   async afterTest() {
