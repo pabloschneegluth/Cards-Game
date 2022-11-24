b0cfeb5725be0fb5e7b5b5f09f7e1eb6115490ae
Pabloooo6 <pschneegluth@edu.tecnocampus.cat>
2022-11-24 11:10:24 +0100

    Backend & Frontend implemented: archer

---
+++ src/www/__test__/villager/Post_20221106_Archer_Context.js
@@ -0,0 +1,43 @@
+import { getByTestId, screen } from "@testing-library/react";
+import { mainView } from "../main";
+import { waitForEnterTheGame, waitForReloadGame } from "../game/actions";
+import { waitForEndMoon } from "../moon/actions";
+import {Names} from "../util/Names";
+import {queryAllStackDigestByCardNames} from "../stack/queries";
+
+export class Post_20221106_Archer_Context {
+  async beforeTest() {
+    // Do your setup here
+    await waitForEnterTheGame();
+  }
+
+  async givenANewGameWithAStackOfNSNSCards(n1, s1, n2, s2) {
+    // text:  * Given a new game with a stack of 1 "Villager", 1 "Bow" cards.
+    // code: await this.givenANewGameWithAStackOfNSNSCards(1, "Villager", 1, "Bow")
+    // hint: Post_20221105_Wizard_Context.givenANewGameWithAStackOfNSNSCards
+
+    await waitForReloadGame();
+  }
+
+  async endTheCurrentMoon() {
+    // text:  * End the current moon.
+    // code: await this.endTheCurrentMoon()
+    // hint: Post_20220913_Wolf_Context.endTheCurrentMoon
+
+    await waitForEndMoon();
+  }
+
+  async thereShouldBeNStacksOfNSCards(expected, n2, s1) {
+    // text:  * There should be 1 stacks of 1 "Archer" cards.
+    // code: await this.thereShouldBeNStacksOfNSCards(1, 1, "Archer")
+    // hint: Post_20221105_Wizard_Context.thereShouldBeNStacksOfNSCards
+
+    var names = Names.byNames(n2, s1).get();
+    var stacks = queryAllStackDigestByCardNames(mainView, names);
+    expect(stacks).toHaveLength(expected);
+  }
+
+  async afterTest() {
+    // Do your teardown here, if necessary
+  }
+}
